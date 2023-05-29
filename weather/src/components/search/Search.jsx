552cacd939b2ea5e6f28dc543a38ee18ce5df586
Sahil Arora <91737464+Sahil-2309@users.noreply.github.com>
2023-05-30 01:41:45 +0530

    search css and jsx

--- weather/src/components/search/Search.jsx
+++ weather/src/components/search/Search.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useState } from 'react'
+import './search.css'
+import '../../index.css'
 
 const Search = () => {
-  return <div>Search</div>
+  const [searchInput, setSearchInput] = useState('')
+  const inputRef = useRef(null)
+  const searchCity = () => {
+    alert(searchInput)
+    setSearchInput('')
+  }
+  const handleChange = (e) => {
+    setSearchInput(e.target.value)
+  }
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      searchCity()
+    }
+  }
+  return (
+    <>
+      <input
+        className='search-bar'
+        type='search'
+        ref={inputRef}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        placeholder='Search City'
+        value={searchInput}
+      ></input>
+      <button type='button' className='btn' onClick={searchCity}>
+        Search
+      </button>
+    </>
+  )
 }
 
 export { Search }
